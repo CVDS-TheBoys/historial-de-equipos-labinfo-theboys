2adfeac14d4b422cf1316552505891e786bf156e
Andrés Ariza <andres-ariza70@hotmail.com>
2022-10-30 14:37:56 -0500

    Se agregó el mapper para Elemento

--- src/main/java/edu/eci/cvds/sampleprj/dao/mybatis/mappers/ElementoMapper.java
+++ src/main/java/edu/eci/cvds/sampleprj/dao/mybatis/mappers/ElementoMapper.java
@@ -1,4 +1,11 @@
 package edu.eci.cvds.sampleprj.dao.mybatis.mappers;
 
+import edu.eci.cvds.samples.entities.Elemento;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface ElementoMapper {
+    public List<Elemento> consultarElementos();
+    public void insertarElemento(@Param("elem") Elemento elemento);
 }
