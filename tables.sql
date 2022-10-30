56a4d061be2d2021166e7da22d437db34c5c0741
JuanPablo70 <juanpasan2000@gmail.com>
2022-10-30 12:23:35 -0500

    Se corrigió el modelo de DB

--- tables.sql
+++ tables.sql
@@ -2,8 +2,12 @@
 -- Tabla LABORATORIOS
 -- -----------------------------------------------------
 CREATE TABLE LABORATORIOS (
-    id			NUMBER(5) NOT NULL,
-    nombre		VARCHAR2(50) NOT NULL
+    id					NUMBER(5) NOT NULL,
+    nombre				VARCHAR2(50) NOT NULL,
+	cantidad_equipos	NUMBER(5) NOT NULL,
+	estado				BOOLEAN NOT NULL,
+	fecha_creacion		DATE NOT NULL,
+	fecha_cierre		DATE
 );
 
 -- -----------------------------------------------------
@@ -11,7 +15,9 @@ CREATE TABLE LABORATORIOS (
 -- -----------------------------------------------------
 CREATE TABLE EQUIPOS (
     id					NUMBER(5) NOT NULL,
-    LABORATORIO_id		NUMBER(5) NOT NULL
+	estado				BOOLEAN NOT NULL,
+    LABORATORIO_id		NUMBER(5) NOT NULL,
+	nombre				VARCHAR2(50) NOT NULL
 );
 
 -- -----------------------------------------------------
@@ -19,9 +25,10 @@ CREATE TABLE EQUIPOS (
 -- -----------------------------------------------------
 CREATE TABLE ELEMENTOS (
     id				NUMBER(5) NOT NULL,
-    tipo			VARCHAR2(50) NOT NULL,
+	nombre			VARCHAR2(50) NOT NULL,
+    tipo			VARCHAR2(10) NOT NULL,
 	funcional		BOOLEAN NOT NULL,
-	EQUIPO_id		NUMBER(5) NOT NULL
+	EQUIPO_id		NUMBER(5)
 );
 
 -- -----------------------------------------------------
