d215a317f675805587aab146b37c859d405b3766
JuanPablo70 <juanpasan2000@gmail.com>
2022-10-30 15:53:24 -0500

    Implementación del DAO para Equipo

---
+++ src/main/java/edu/eci/cvds/sampleprj/dao/EquipoDAO.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.sampleprj.dao;
+
+import edu.eci.cvds.samples.entities.Equipo;
+import org.apache.ibatis.exceptions.PersistenceException;
+
+import java.util.List;
+
+public interface EquipoDAO {
+    public void save(Equipo e) throws PersistenceException;
+    public Equipo load(int id) throws PersistenceException;
+    public List<Equipo> loadAll() throws PersistenceException;
+
+}
---
+++ src/main/java/edu/eci/cvds/sampleprj/dao/mybatis/MyBATISEquipoDAO.java
@@ -0,0 +1,46 @@
+package edu.eci.cvds.sampleprj.dao.mybatis;
+
+import com.google.inject.Inject;
+import edu.eci.cvds.sampleprj.dao.EquipoDAO;
+import edu.eci.cvds.sampleprj.dao.mybatis.mappers.EquipoMapper;
+import edu.eci.cvds.samples.entities.Equipo;
+import org.apache.ibatis.exceptions.PersistenceException;
+
+import java.util.List;
+
+public class MyBATISEquipoDAO implements EquipoDAO {
+    @Inject
+    private EquipoMapper equipoMapper;
+
+    @Override
+    public void save(Equipo equipo) throws PersistenceException {
+        try {
+            equipoMapper.insertarEquipo(equipo);
+        } catch (org.apache.ibatis.exceptions.PersistenceException e) {
+            System.out.println(e.getMessage());
+            throw new org.apache.ibatis.exceptions.PersistenceException("Error al registrar el equipo " + equipo.toString(), e);
+        }
+    }
+
+    @Override
+    public Equipo load(int id) throws PersistenceException {
+        try {
+            return equipoMapper.consultarEquipo(id);
+        } catch (org.apache.ibatis.exceptions.PersistenceException e) {
+            System.out.println(e.getMessage());
+            throw new org.apache.ibatis.exceptions.PersistenceException("Error al consultar el equipo " + id, e);
+        }
+    }
+
+    @Override
+    public List<Equipo> loadAll() throws PersistenceException {
+        try {
+            return equipoMapper.consultarEquipos();
+        } catch (org.apache.ibatis.exceptions.PersistenceException e) {
+            System.out.println(e.getMessage());
+            throw new org.apache.ibatis.exceptions.PersistenceException("Error al consultar los equipos ", e);
+        }
+    }
+
+
+}
