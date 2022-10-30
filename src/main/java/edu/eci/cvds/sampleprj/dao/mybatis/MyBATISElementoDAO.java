46cb2cd2f291608f4f7f297f68caf8b2b1feac28
Andrés Ariza <andres-ariza70@hotmail.com>
2022-10-30 14:40:17 -0500

    Implementación del DAO para Elemento

--- src/main/java/edu/eci/cvds/sampleprj/dao/mybatis/MyBATISElementoDAO.java
+++ src/main/java/edu/eci/cvds/sampleprj/dao/mybatis/MyBATISElementoDAO.java
@@ -1,4 +1,38 @@
 package edu.eci.cvds.sampleprj.dao.mybatis;
 
-public class MyBATISElementoDAO {
+import com.google.inject.Inject;
+import edu.eci.cvds.sampleprj.dao.ElementoDAO;
+import edu.eci.cvds.sampleprj.dao.mybatis.mappers.ElementoMapper;
+import edu.eci.cvds.samples.entities.Elemento;
+
+import org.apache.ibatis.exceptions.PersistenceException;
+import java.util.List;
+
+public class MyBATISElementoDAO implements ElementoDAO {
+    @Inject
+    private ElementoMapper elementoMapper;
+
+    @Override
+    public Elemento load(int id) throws PersistenceException {
+        return null;
+    }
+
+    @Override
+    public void save(Elemento elemento) throws PersistenceException {
+        try {
+            elementoMapper.insertarElemento(elemento);
+        } catch (org.apache.ibatis.exceptions.PersistenceException e) {
+            System.out.println(e.getMessage());
+            throw new org.apache.ibatis.exceptions.PersistenceException("Error al registrar el elemento " + elemento.toString(), e);
+        }
+    }
+
+    @Override
+    public List<Elemento> loadAll() throws PersistenceException {
+        try {
+            return elementoMapper.consultarElementos();
+        } catch (org.apache.ibatis.exceptions.PersistenceException e) {
+            throw new org.apache.ibatis.exceptions.PersistenceException("Error al consultar los elementos disponibles " + e);
+        }
+    }
 }
--- src/main/java/edu/eci/cvds/samples/entities/Elemento.java
+++ src/main/java/edu/eci/cvds/samples/entities/Elemento.java
@@ -1,4 +1,82 @@
 package edu.eci.cvds.samples.entities;
 
+/**
+ * @author Ariza
+ */
 public class Elemento {
+
+    private int id;
+    private String nombre;
+    private String tipo;
+    private boolean funcional;
+    private Integer equipo_id;
+
+    // TODO private List<Novedad> novedades
+
+    public Elemento(int id, String nombre, String tipo, boolean funcional) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.funcional = funcional;
+        this.equipo_id = -1;
+    }
+
+    public Elemento(int id, String nombre, String tipo, boolean funcional, Integer equipo_id) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.funcional = funcional;
+        this.equipo_id = equipo_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public boolean isFuncional() {
+        return funcional;
+    }
+
+    public void setFuncional(boolean funcional) {
+        this.funcional = funcional;
+    }
+
+    public int getEquipo_id() {
+        return equipo_id;
+    }
+
+    public void setEquipo_id(int equipo_id) {
+        this.equipo_id = equipo_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Elemento{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", tipo='" + tipo + '\'' +
+                ", funcional=" + funcional +
+                ", Equipo_id=" + equipo_id +
+                '}';
+    }
 }
