9b12543a4044f5e733bd9adf7e8878fbf819bc3a
JuanPablo70 <juanpasan2000@gmail.com>
2022-11-01 19:48:49 -0500

    Se cambió la implementación para equipos

---
+++ src/main/java/edu/eci/cvds/samples/services/ServiciosEquipo.java
@@ -0,0 +1,20 @@
+package edu.eci.cvds.samples.services;
+
+import edu.eci.cvds.samples.entities.Equipo;
+
+import java.util.List;
+
+public interface ServiciosEquipo {
+    /**
+     * Consulta todos los equipos registrados
+     * @return lista de equipos registrados
+     * @throws ExcepcionServiciosLaboratorio si no puede cargar los equipos
+     */
+    public List<Equipo> consultarEquipos() throws ExcepcionServiciosLaboratorio;
+    /**
+     * Registra un nuevo equipo
+     * @param equipo elemento a registrar
+     * @throws ExcepcionServiciosLaboratorio si el identificador del equipo ya existe
+     */
+    public void registrarEquipo(Equipo equipo) throws ExcepcionServiciosLaboratorio;
+}
---
+++ src/main/java/edu/eci/cvds/samples/services/impl/ServiciosEquipoImpl.java
@@ -0,0 +1,33 @@
+package edu.eci.cvds.samples.services.impl;
+
+import com.google.inject.Inject;
+import edu.eci.cvds.sampleprj.dao.EquipoDAO;
+import edu.eci.cvds.samples.entities.Equipo;
+import edu.eci.cvds.samples.services.ExcepcionServiciosLaboratorio;
+import edu.eci.cvds.samples.services.ServiciosEquipo;
+import org.apache.ibatis.exceptions.PersistenceException;
+
+import java.util.List;
+
+public class ServiciosEquipoImpl implements ServiciosEquipo {
+    @Inject
+    private EquipoDAO equipoDAO;
+
+    @Override
+    public List<Equipo> consultarEquipos() throws ExcepcionServiciosLaboratorio {
+        try {
+            return equipoDAO.loadAll();
+        } catch (PersistenceException ex) {
+            throw new ExcepcionServiciosLaboratorio("Error al cargar los equipos " + ex);
+        }
+    }
+
+    @Override
+    public void registrarEquipo(Equipo equipo) throws ExcepcionServiciosLaboratorio {
+        try {
+            equipoDAO.save(equipo);
+        } catch (PersistenceException ex) {
+            throw new ExcepcionServiciosLaboratorio("Error al registrar el equipo " + equipo.getId() + ex);
+        }
+    }
+}
