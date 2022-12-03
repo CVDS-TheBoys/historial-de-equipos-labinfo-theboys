82867c8c23a28cd3301f592b814caf152f041ae6
Andrés Ariza <andres-ariza70@hotmail.com>
2022-12-03 10:19:01 -0500

    Creación Servicios Usuario

---
+++ src/main/java/edu/eci/cvds/services/impl/ServiciosUsuarioImpl.java
@@ -0,0 +1,22 @@
+package edu.eci.cvds.services.impl;
+
+import com.google.inject.Inject;
+import edu.eci.cvds.entities.Usuario;
+import edu.eci.cvds.persistence.UsuarioDAO;
+import edu.eci.cvds.services.ExcepcionServiciosLaboratorio;
+import edu.eci.cvds.services.ServiciosUsuario;
+import org.apache.ibatis.exceptions.PersistenceException;
+
+public class ServiciosUsuarioImpl implements ServiciosUsuario {
+    @Inject
+    private UsuarioDAO usuarioDAO;
+
+    @Override
+    public Usuario consultarUsuario(String email, String contra) throws ExcepcionServiciosLaboratorio {
+        try {
+            return usuarioDAO.load(email, contra);
+        } catch (PersistenceException ex) {
+            throw new ExcepcionServiciosLaboratorio("Error al cargar usuario " + ex);
+        }
+    }
+}
